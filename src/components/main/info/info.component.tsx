beb4481e4b0aaa20730e9b593ce24f8d76cbedd7
leeeunseodd <dlwltn1283@naver.com>
2025-01-07 14:51:02 +0900

    수정

--- src/components/main/info/info.component.tsx
+++ src/components/main/info/info.component.tsx
@@ -87,11 +87,13 @@ export default function InfoItemPage() {
 		<div className={styles.info_view}>
 			{/* about*/}
 			<div className={`${styles.info_detail} ${styles.info_container}`}>
-				<h1 className={styles.info_title}>CRAFTING DIGITAL EXPERIENCES ALL AROUND THE GLOB</h1>
-				<h2 className={styles.info_title}>ABOUT</h2>
+				<h1 className={styles.info_title}>저를 소개합니다 두근두근 하하하</h1>
 				<div className={styles.sub_title}>
-					<h2>CRAFTING DIGITAL EXPERIENCES ALL AROUND THE GLOB</h2>
-					<h3>Lab21 is an independent web development studio based in Athens, Greece with worldwide projects. We specialize in UX driven custom websites, portals, blogs and all aspects of web development.</h3>
+					<h2>ABOUT</h2>
+					<div className={styles.info_cont}>
+						<h3>전 세계의 래프팅 디지털 경험</h3>
+						<p>Lab21은 그리스 아테네에 본사를 둔 독립 웹 개발 스튜디오로, 전 세계적인 프로젝트를 진행하고 있습니다. 저희는 UX 기반 맞춤형 웹사이트, 포털, 블로그 및 웹 개발의 모든 측면을 전문으로 하고 있습니다.</p>
+					</div>
 				</div>
 			</div>
 			{/* skill*/}
@@ -99,12 +101,14 @@ export default function InfoItemPage() {
 				<div className={styles.skill_title}>
 					<h2>My Skill</h2>
 					<h3>WHAT I CAN DO FOR YOUR</h3>
-					<p>비즈니스의 발전, 사용자의 아이디어를 현실로 바꾸는 창의적인 기술 및 스타일을 받아들일 자세와 지식이 준비되어있습니다.</p>
-					<p>저의 기술에 자부심을 가지고 있으며, 기술뿐만 아니라 기획 디자인 개발 등 마지막까지 충실하며사용자 경험과 사용성을 중심으로 웹사이트를 구축합니다.</p>
-					<p>저의 목표는 성과가 아닌, 사용자 중심의 경험을 만들고 고객의 목표달성입니다.</p>
 				</div>
-				<div className={styles.skill_list}>
-					<ul>
+				<div className={styles.skill_cont}>
+					<div className={styles.sub_title}>
+						<p>비즈니스의 발전, 사용자의 아이디어를 현실로 바꾸는 창의적인 기술 및 스타일을 받아들일 자세와 지식이 준비되어있습니다.</p>
+						<p>저의 기술에 자부심을 가지고 있으며, 기술뿐만 아니라 기획 디자인 개발 등 마지막까지 충실하며사용자 경험과 사용성을 중심으로 웹사이트를 구축합니다.</p>
+						<p>저의 목표는 성과가 아닌, 사용자 중심의 경험을 만들고 고객의 목표달성입니다.</p>
+					</div>
+					<ul className={styles.skill_list}>
 						<li>JavaScript</li>                 
 						<li>React & Next.js</li>
 						<li>Typescript</li>
