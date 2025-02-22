55077c52be5e7294f620f54a0f2e79f073286bfb
leeeunseodd <dlwltn1283@naver.com>
2025-02-22 21:52:36 +0900

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 import path from 'path';
 
@@ -6,11 +7,11 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/public')],
-    //prependData: `@import "@styles/abstracts/variables.scss";`,
-   // prependData: `@use "public/styles/variables as *";`,
   },
   webpack: (config) => {
     config.resolve.alias['@styles'] = path.join(__dirname, 'src/styles');
+    // locomotive-scroll은 클라이언트에서만 사용되도록 설정
+    config.resolve.alias['locomotive-scroll'] = path.join(__dirname, 'node_modules/locomotive-scroll/dist/locomotive-scroll.js');
     return config;
   },
   images: {
