f121e1c2b60162cff1e725eb10efc1cbb4188b1e
leeeunseodd <dlwltn1283@naver.com>
2025-02-22 21:51:08 +0900

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,6 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.alias['@styles'] = path.join(__dirname, 'src/styles');
-    config.resolve.alias['locomotive-scroll'] = require.resolve('locomotive-scroll');
     return config;
   },
   images: {
