58a27ae2afe543b2ff89cd3a6483320d56659a21
lEOOEE <ieunseo802@gmail.com>
2025-06-11 13:08:11 +0900

    okay

--- src/components/main/home/home.component.tsx
+++ src/components/main/home/home.component.tsx
@@ -1,11 +1,6 @@
 'use client'
 
 import { useState } from 'react';
-import { motion } from 'framer-motion';
-import { useLoader } from './hooks/useLoader';
-
-import BackgroundMotion from '@components/_utiles/parallax/backgroundMotion.component';
-import Loader from '@components/_utiles/loader/loader.component';
 import NavComponent from '@components/sementic/navigation/nav.component';
 import { IntroSection } from '@components/main/intro/intro.component'
 import { ProjectSection } from '@components/main/work/components/project'
@@ -16,30 +11,15 @@ import styles from './home.module.scss';
 
 // start
 export default function HomeItemPage() {
-  const { showContent, handleLoaderComplete } = useLoader();
-  const [isBackgroundInView01, setIsBackgroundInView01] = useState(false);
-  const [isBackgroundInView02, setIsBackgroundInView02] = useState(false);
-  {/* <BackgroundMotion onInView={setIsBackgroundInView01}>
-  </BackgroundMotion> */}
-
   return (
     <>
-      {/* loader */}
-      <Loader variant="blind_pastel_blue" onComplete={handleLoaderComplete} className={''} />
-      <motion.div
-        className={`${styles.home_detail} ${isBackgroundInView01 ? styles.active01 : ''} ${isBackgroundInView02 ? styles.active02 : ''}`}
-        initial={{ display: 'none', y: '100%' }}
-        animate={{ display: 'block', y: showContent ? 0 : '100%', transition: { duration: 0.6, ease: 'easeOut' } }}
-      >
-        <NavComponent />
-        <IntroSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectSection />
-        <ArchivingSection />
-        <GuestSection />
-      </motion.div>
-
+      <NavComponent />
+      <IntroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectSection />
+      <ArchivingSection />
+      <GuestSection />
     </>
   );
 }
