32f38283dc20cc04699114d897c29ff1011648d0
lEOOEE <ieunseo802@gmail.com>
2025-06-11 16:44:13 +0900

    ...

--- src/lib/session/userInfo.ts
+++ src/lib/session/userInfo.ts
@@ -8,6 +8,6 @@ export const authOptions: AuthOptions = {
       clientId: process.env.AUTH_GITHUB_ID as string,
       clientSecret: process.env.AUTH_GITHUB_SECRET as string,
     }),
-  
+  ],
   secret: process.env.NEXTAUTH_JWT,
 };
