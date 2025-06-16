79e873151cd1c25531d420fbe81b40a21e6324f3
lEOOEE <ieunseo802@gmail.com>
2025-06-16 17:36:30 +0900

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@
 
 // import type { Metadata } from 'next'
 import { Suspense } from 'react';
+import { Metadata } from 'next';
 import Loading from './loading';
 import ErrorWrapper from '@lib/error/errorWrapper.boundary';
 import { AlertProvider } from '@lib/alert/alert.context';
 import { LoadingProvider } from '@lib/loading/loading.context';
 import { ResponsiveProvider } from '@lib/context/responsive.context';
-import { SessionProvider } from "next-auth/react"
-import './globals.scss'
+import { SessionProvider } from "next-auth/react";
+import './globals.scss';
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
 
