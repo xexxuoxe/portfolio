092b8ac3098c5c11624a60377812339281e1fbdc
lEOOEE <ieunseo802@gmail.com>
2025-09-01 15:02:12 +0900

    ...

--- src/components/m/main/archiving/constants/archiveList.ts
+++ src/components/m/main/archiving/constants/archiveList.ts
@@ -12,8 +12,8 @@ export const archiveData = [
   {
     img: '/images/ico_xexxuoxe2.png',
     alt: 'threed',
-    url: '죄송합니다. 8월 오픈예정입니다.',
-    description: '개인 기술 블로그 (8월 오픈 예정)',
+    url: '죄송합니다. 10월 오픈예정입니다.',
+    description: '개인 기술 블로그 (10월 오픈 예정)',
     class: 'blog',
   },
 ];
